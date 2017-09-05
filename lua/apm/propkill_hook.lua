fe6cefed9692d7aa89a90e91f5ab285fc7c713ac
joeyjumper94 <joeyjumper94@gmail.com>
2017-09-05 12:29:05 -0700

    shortened the kill messages
    i noticed that if the players have long names, the name of the second
    player may be cut off, shortening the messages can help wit that

--- lua/apm/propkill_hook.lua
+++ lua/apm/propkill_hook.lua
@@ -1,15 +1,14 @@
 local MurderEntBlackList={"prop_physics","gmod_balloon","gmod_button","gmod_hoverball","gmod_thruster"}
-CreateConVar("apm_prop_suicide_notification", "1", {FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE}, "decide whether to notify admins if someone is killed by their own prop")
 if SERVER then
 	function GAMEMODE:PlayerDeath(ply,weapon,killer)--the hook to define the killer and the victim
 		if IsValid(weapon) and table.concat(MurderEntBlackList):find(weapon:GetClass()) then
 			if weapon:CPPIGetOwner()!=ply or GetConVarNumber("apm_prop_suicide_notification")!=0 then
 				if ConVarExists("ulx_rslots") then-- check if ulx is installed
-					RunConsoleCommand("ulx","asay", "you may want to spectate someone because an entity owned by "..weapon:CPPIGetOwner():Nick().." ("..weapon:CPPIGetOwner():SteamID()..") killed "..ply:Nick().." ("..ply:SteamID()..")")
+					RunConsoleCommand("ulx","asay", "someone spectate "..weapon:CPPIGetOwner():Nick().." ("..weapon:CPPIGetOwner():SteamID().."). A prop they own killed "..ply:Nick().." ("..ply:SteamID()..").")
 				else
 					for k,v in pairs(player.GetAll()) do
 						if v:IsAdmin() then
-							v:PrintMessage(HUD_PRINTTALK, "you may want to spectate someone because an entity owned by "..weapon:CPPIGetOwner():Nick().." ("..weapon:CPPIGetOwner():SteamID()..") killed "..ply:Nick().." ("..ply:SteamID()..")")
+							v:PrintMessage(HUD_PRINTTALK, "someone spectate "..weapon:CPPIGetOwner():Nick().." ("..weapon:CPPIGetOwner():SteamID().."). A prop they own killed "..ply:Nick().." ("..ply:SteamID()..").")
 						end
 					end
 				end
