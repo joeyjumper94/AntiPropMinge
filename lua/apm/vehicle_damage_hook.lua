e074db87f4f582882222d6804c4e15cfed3b0ae8
Hackcraft <zakary@zakary.org>
2017-09-12 16:59:10 +0100

    Removed unnecessary check
    IsVehicle is enough.

--- lua/apm/vehicle_damage_hook.lua
+++ lua/apm/vehicle_damage_hook.lua
@@ -1,7 +1,7 @@
 hook.Add("PlayerShouldTakeDamage", "APM_vehicle_damage", function(ply, weapon)
-	if IsValid(weapon) and (weapon:GetClass():find("vehicle") or weapon:IsVehicle()) then
-		local driver=weapon:GetDriver()
-		if driver:IsValid() and driver:IsPlayer() and GetConVarNumber("apm_notify_on_vehicle_damage")!=0 then
+	if IsValid(weapon) and weapon:IsVehicle() then
+		local driver = weapon:GetDriver()
+		if driver:IsValid() and driver:IsPlayer() and GetConVarNumber("apm_notify_on_vehicle_damage") !=0 then
 			if ConVarExists("ulx_rslots") then
 				RunConsoleCommand("ulx", "asay", "someone spectate ".. driver:Nick().. " ("..driver:SteamID().. "). A vehicle they're in damaged ".. ply:Nick().. " (".. ply:SteamID().. ").")
 			else
